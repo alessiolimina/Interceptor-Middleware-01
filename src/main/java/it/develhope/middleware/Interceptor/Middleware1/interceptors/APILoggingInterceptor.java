3fa758fd720f8b7c852f541ae54115202c0c808a
Alessio Limina <alessio.limina@gmail.com>
2023-04-28 23:17:40 +0200

    Exercise Middleware 01 updated, to be completed

--- src/main/java/it/develhope/middleware/Interceptor/Middleware1/interceptors/APILoggingInterceptor.java
+++ src/main/java/it/develhope/middleware/Interceptor/Middleware1/interceptors/APILoggingInterceptor.java
@@ -1,5 +1,6 @@
 package it.develhope.middleware.Interceptor.Middleware1.interceptors;
 
+import it.develhope.middleware.Interceptor.Middleware1.controllers.BasicController;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
@@ -8,15 +9,19 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Component
 public class APILoggingInterceptor implements HandlerInterceptor {
-
+    @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.println(request.getHeader("User-Agent"));
         return true;
     }
 
+    @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
 
+    @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
     }
 }
--- src/main/java/it/develhope/middleware/Interceptor/Middleware1/interceptors/LegacyInterceptor.java
+++ src/main/java/it/develhope/middleware/Interceptor/Middleware1/interceptors/LegacyInterceptor.java
@@ -9,17 +9,20 @@ import org.springframework.web.servlet.ModelAndView;
 @Component
 public class LegacyInterceptor implements HandlerInterceptor {
 
-    @Override
+
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return HandlerInterceptor.super.preHandle(request, response, handler);
+        if(request.getRequestURL().toString().contains("/legacy")){
+            response.setStatus(410);
+        }
+        return false;
     }
 
-    @Override
+
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
 
-    @Override
+
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
     }
